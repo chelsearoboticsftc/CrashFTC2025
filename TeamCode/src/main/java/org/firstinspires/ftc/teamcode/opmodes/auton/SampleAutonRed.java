f126b65e8fd962697e0b329d496085a070fd9d15
Gremlin <fjzempel@gmail.com>
2025-10-06 19:34:43 -0400

    Updated Team Red Auton for Potential Use
    Maybe it works this is for a test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SampleAutonRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SampleAutonRed.java
@@ -1,7 +1,23 @@
 package org.firstinspires.ftc.teamcode.opmodes.auton;
 
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Autonomous
 public class SampleAutonRed extends LinearOpMode{
@@ -10,8 +26,10 @@ public class SampleAutonRed extends LinearOpMode{
 
         waitForStart();
 
+            // instantiate your MecanumDrive at a particular pose.
+            Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+            MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
     }
 
 
 }
-
