a5e74693887c9dd87ebba78af8166643f8b2cd94
eidoo1gaN <vaughnwbrodie@gmail.com>
2025-11-01 08:06:50 -0400

    Started Close Auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/CloseAutonRed.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmodes.auton;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.example.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.example.ShooterSubsystem;
+
+@Autonomous
+public class CloseAutonRed extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        ShooterSubsystem shooter = new ShooterSubsystem(hardwareMap);
+        Intake intake = new Intake(hardwareMap);
+        
+        waitForStart();
+        while(opModeIsActive()){
+            Actions.runBlocking(
+            drive.actionBuilder(new Pose2d(0, 0, 0))
+                    .lineToX(30)
+                    .build());
+
+
+        }
+
+
+    }
+}
